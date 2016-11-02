02e37c150f86c5bebfd250bd3320610bf473a0c0
ozPop <ozziepopovas@gmail.com>
2016-11-02 17:00:49 -0400

    add description and inventory displaying

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,15 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? 'true' : 'false'
+  end
 end
